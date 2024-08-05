c760f6fdaf54ce928d24be64295b8b78982f4021
Tchantcho-tchoutto-isaac <94875081+Tchantcho-tchoutto-isaac@users.noreply.github.com>
2024-08-06 00:36:19 +0200

    KAN-20:Historique des Lots d'un utisateur

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -7,16 +7,19 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Repository\TicketRepository;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\JsonResponse;
 class TicketController extends AbstractController
+
 {
     private $doctrine;
 
-    public function __construct(ManagerRegistry $doctrine)
+    public function __construct(ManagerRegistry $doctrine , private TicketRepository $ticketRepository  , private Security $security)
     {
         $this->doctrine = $doctrine;
     }
-
+    
     #[Route('/tikets', name: 'tikets',)]
     public function index(): Response
     {
@@ -42,5 +45,33 @@ class TicketController extends AbstractController
             'lotCounts' => $lotCounts,
             'lotTickets' => $lotTickets,
         ]);
+    }
+
+    
+    
+     #[Route('api/user/lot', name: 'user_prizes', methods:['get'])]
+    public function getUserPrizes(): JsonResponse
+    {
+        $user = $this->security->getUser();
+
+        if (!$user) {
+            return new JsonResponse(['message' => 'User not authenticated'], 401);
+        }
+
+        $tickets = $this->ticketRepository->findBy(['user' => $user]);
+
+        $prizes = [];
+        foreach ($tickets as $ticket) {
+            $lot = $ticket->getLot();
+            if ($lot) {
+                $prizes[] = [
+                    'code' => $ticket->getCode(),
+                    'prize_name' => $lot->getName(),
+                    'prize_value' => $lot->getValue(),
+                ];
+            }
+        }
+
+        return new JsonResponse($prizes);
     }
 }
